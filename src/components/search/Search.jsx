188f9ab0bf4672d23bf6c301512eab185be7d449
Ozanne94 <ozanne.anthony@hotmail.fr>
2023-07-27 17:48:56 +0200

    Partie statique composant Search terminée

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,8 +1,25 @@
 import React from "react";
 import "./Search.scss";
+import Avatar from "@mui/material/Avatar";
 
 const Search = () => {
-  return <div>Search</div>;
+  return (
+    <div className="search">
+      <div className="searchForm">
+        <input type="text" placeholder="Recherchez une personne" />
+      </div>
+      <div className="userChat">
+        <Avatar
+          className="avatar"
+          alt="Masha"
+          src="https://images.pexels.com/photos/2726111/pexels-photo-2726111.jpeg?auto=compress&cs=tinysrgb&w=1600"
+        />
+        <div className="userChatInfo">
+          <span>Masha</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Search;
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./Sidebar.scss";
 import Navbar from "../navbar/Navbar";
+import Search from "../search/Search";
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
       <Navbar />
+      <Search />
     </div>
   );
 };
