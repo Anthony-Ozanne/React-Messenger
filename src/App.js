a6ffdd06ccabc0cb5c1e69805ec09acc88f7dd5a
Ozanne94 <ozanne.anthony@hotmail.fr>
2023-07-27 11:43:55 +0200

    Création des premiers composants + ajout de react-router ok

--- src/App.js
+++ src/App.js
@@ -1,5 +1,44 @@
+import Home from "./components/home/Home";
+import Login from "./components/login/Login";
+import Register from "./components/register/Register";
+
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+
 function App() {
-  return <div>Hello World!</div>;
+  const Layout = () => {
+    return (
+      <div className="main">
+        <div className="container">
+          <div className="contentContainer">
+            <Outlet />
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+      ],
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/register",
+      element: <Register />,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
