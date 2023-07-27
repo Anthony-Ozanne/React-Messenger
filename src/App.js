82f24f69b35fcf4ca276eaecea5740fca54f915d
Ozanne94 <ozanne.anthony@hotmail.fr>
2023-07-27 13:14:58 +0200

    Ajout du style pour ma page Register

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
 
+import "./style.scss";
+
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 
 function App() {
