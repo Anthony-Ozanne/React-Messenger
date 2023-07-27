b9261f76b90414bdee75d3e957c717834ca42095
Ozanne94 <ozanne.anthony@hotmail.fr>
2023-07-27 15:27:09 +0200

    Partie statique de la page register terminé

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import AddPhotoAlternateOutlinedIcon from "@mui/icons-material/AddPhotoAlternateOutlined";
 import "./Register.scss";
 
 const Register = () => {
@@ -8,10 +9,14 @@ const Register = () => {
         <span className="logo">React Messenger</span>
         <h4 className="title">Je veux m'enregistrer</h4>
         <form>
-          <input type="text" placeholder="display name"></input>
-          <input type="email" placeholder="email"></input>
-          <input type="password" placeholder="password"></input>
-          <input type="file"></input>
+          <input type="text" placeholder="Nom"></input>
+          <input type="email" placeholder="Email"></input>
+          <input type="password" placeholder="Mot de passe"></input>
+          <input style={{ display: "none" }} type="file" id="file" />
+          <label htmlFor="file">
+            <AddPhotoAlternateOutlinedIcon className="icon" />
+            <span>Ajouter une image de profil</span>
+          </label>
           <button>S'enregistrer</button>
         </form>
         <p>Vous avez déjà un compte ? Connectez-vous</p>
