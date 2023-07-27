f49a381657f4d66f4ad797f4bfbcbf85a35ce817
Ozanne94 <ozanne.anthony@hotmail.fr>
2023-07-27 15:33:10 +0200

    Partie statique de la page login terminé

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -2,7 +2,20 @@ import React from "react";
 import "./Login.scss";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="formContainer">
+      <div className="formWrapper">
+        <span className="logo">React Messenger</span>
+        <h4 className="title">Je me connecte</h4>
+        <form>
+          <input type="email" placeholder="Email"></input>
+          <input type="password" placeholder="Mot de passe"></input>
+          <button>Se connecter</button>
+        </form>
+        <p>Vous n'avez pas de compte ? Créer mon compte</p>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
