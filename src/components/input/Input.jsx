162e9960a73091de90020da9609534339bb7b684
Ozanne94 <ozanne.anthony@hotmail.fr>
2023-07-27 19:41:12 +0200

    Composant Input statique terminé

--- src/components/input/Input.jsx
+++ src/components/input/Input.jsx
@@ -1,8 +1,22 @@
 import React from "react";
 import "./Input.scss";
+import AttachEmailOutlinedIcon from "@mui/icons-material/AttachEmailOutlined";
+import AddPhotoAlternateOutlinedIcon from "@mui/icons-material/AddPhotoAlternateOutlined";
 
 const Input = () => {
-  return <div className="input">Input</div>;
+  return (
+    <div className="input">
+      <input type="text" placeholder="Ecrivez votre message ..." />
+      <div className="send">
+        <AttachEmailOutlinedIcon className="attach" />
+        <input type="file" style={{ display: "none" }} id="file" />
+        <label htmlFor="file">
+          <AddPhotoAlternateOutlinedIcon className="AddPhoto" />
+        </label>
+        <button>Envoyer</button>
+      </div>
+    </div>
+  );
 };
 
 export default Input;
