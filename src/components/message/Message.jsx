1ba7612ccd4ab9c3375ca24f90089db26d972782
Ozanne94 <ozanne.anthony@hotmail.fr>
2023-07-28 10:50:17 +0200

    Partie statique de l'application terminée
    Je passe maintenant à la partie logique

--- src/components/message/Message.jsx
+++ src/components/message/Message.jsx
@@ -1,8 +1,27 @@
 import React from "react";
 import "./Message.scss";
+import Avatar from "@mui/material/Avatar";
 
 const Message = () => {
-  return <div>Message</div>;
+  return (
+    <div className="message owner">
+      <div className="messageInfo">
+        <Avatar
+          className="avatar"
+          alt="Remy Sharp"
+          src="https://images.pexels.com/photos/1043471/pexels-photo-1043471.jpeg?auto=compress&cs=tinysrgb&w=1600"
+        />
+        <span>à l'instant</span>
+      </div>
+      <div className="messageContent">
+        <p>Bonjour</p>
+        <img
+          src="https://images.pexels.com/photos/1043471/pexels-photo-1043471.jpeg?auto=compress&cs=tinysrgb&w=1600"
+          alt="Remy"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Message;
