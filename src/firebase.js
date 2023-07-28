893a64540fd19dc5ff22d20a335209b86b0362b7
Ozanne94 <ozanne.anthony@hotmail.fr>
2023-07-28 23:27:24 +0200

    Mis en place de Firestore en cours

---
+++ src/firebase.js
@@ -0,0 +1,29 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAf7l4oKYPYXuLs_fExE_O_zsCafXRnhcA",
+
+  authDomain: "react-messenger-56940.firebaseapp.com",
+
+  databaseURL:
+    "https://react-messenger-56940-default-rtdb.europe-west1.firebasedatabase.app",
+
+  projectId: "react-messenger-56940",
+
+  storageBucket: "react-messenger-56940.appspot.com",
+
+  messagingSenderId: "453935944510",
+
+  appId: "1:453935944510:web:5d62f6d29421a71bdf126f",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
+
+// Create a root reference
+export const storage = getStorage();
+export const db = getFirestore();
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,14 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import AddPhotoAlternateOutlinedIcon from "@mui/icons-material/AddPhotoAlternateOutlined";
 import "./Register.scss";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { auth, storage, db } from "../../firebase";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { doc, setDoc } from "firebase/firestore";
 
 const Register = () => {
+  const [error, setError] = useState(false);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const name = event.target[0].value;
+    const email = event.target[1].value;
+    const password = event.target[2].value;
+    const file = event.target[3].files[0];
+
+    try {
+      const response = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+
+      const storageRef = ref(storage, name);
+
+      const uploadTask = uploadBytesResumable(storageRef, file);
+
+      // Register three observers:
+      // 1. 'state_changed' observer, called any time the state changes
+      // 2. Error observer, called on failure
+      // 3. Completion observer, called on successful completion
+      uploadTask.on(
+        (error) => {
+          setError(true);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+            await updateProfile(response.user, {
+              name,
+              photoURL: downloadURL,
+            });
+            await setDoc(doc(db, "users", response.user.uid), {
+              uid: response.user.uid,
+              name,
+              email,
+              photoURL: downloadURL,
+            });
+          });
+        }
+      );
+    } catch (error) {
+      setError(true);
+    }
+  };
+
   return (
     <div className="formContainer">
       <div className="formWrapper">
         <span className="logo">React Messenger</span>
         <h4 className="title">Je veux m'enregistrer</h4>
-        <form>
+        {error && <span>Quelque chose ne marche pas comme prévu</span>}
+        <form onSubmit={handleSubmit}>
           <input type="text" placeholder="Nom"></input>
           <input type="email" placeholder="Email"></input>
           <input type="password" placeholder="Mot de passe"></input>
